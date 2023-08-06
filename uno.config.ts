09d64874d4495ac32e91d124c18fb2f43bf4bbcd
Santiago Garcia <santiago.garcia@posteo.de>
2023-08-06 09:57:21 +0200

    Desktop solution

---
+++ uno.config.ts
@@ -0,0 +1,39 @@
+// uno.config.ts
+import {
+    defineConfig,
+    presetAttributify,
+    presetIcons,
+    presetTypography, presetUno, presetWebFonts,
+    transformerDirectives, transformerVariantGroup
+} from 'unocss'
+
+export default defineConfig({
+    shortcuts: [
+        // ...
+    ],
+    theme: {
+        colors: {
+            cream: 'hsl(30, 38%, 92%)',
+            darkcyan: 'hsl(158, 36%, 37%)',
+            darkblue: 'hsl(212, 21%, 14%)',
+            grayishblue: 'hsl(228, 12%, 48%)'
+        }
+    },
+    presets: [
+        presetUno(),
+        presetAttributify(),
+        presetIcons(),
+        presetTypography(),
+        presetWebFonts({
+            provider: 'google',
+            fonts: {
+                sans: 'Montserrat:500,700',
+                serif: 'Fraunces:700'
+            },
+        }),
+    ],
+    transformers: [
+        transformerDirectives(),
+        transformerVariantGroup(),
+    ],
+})
