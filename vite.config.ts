54441175faf2ed102f76a6744cfc164ca95799e8
Santiago Garcia <santiago.garcia@posteo.de>
2023-08-06 11:47:20 +0200

    Prepare deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    base: '/fm-product-preview-card/',
     plugins: [
         UnoCSS(),
     ],
